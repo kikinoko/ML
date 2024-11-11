cbcb21d079945c2ae3eb81f6e81f2655904f7b8d
きのこ <kinokonokaibutu@gmail.com>
2024-11-11 16:59:49 +0900

    コメント移動,txt追加

--- ex41.py
+++ ex41.py
@@ -1,17 +1,5 @@
 def newton_method(f, f_prime, x0, tol=1e-7, max_iter=100):
-    """
-    ニュートン法で f(x) = 0 の解を求める
-
-    Parameters:
-    f (function): 関数 f(x)
-    f_prime (function): 関数 f(x) の導関数 f'(x)
-    x0 (float): 初期値
-    tol (float): 許容誤差
-    max_iter (int): 最大反復回数
-
-    Returns:
-    float: 近似解
-    """
+   
     x = x0
     for i in range(max_iter):
         fx = f(x)
@@ -30,7 +18,7 @@ def newton_method(f, f_prime, x0, tol=1e-7, max_iter=100):
     
     return x
 
-# 例: f(x) = x^2 - 2 の解を求める
+#  f(x) = x^2 - 2 の解を求める
 f = lambda x: x**2 - 2
 f_prime = lambda x: 2 * x
 x0 = 1.0  # 初期値
